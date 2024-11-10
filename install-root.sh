f066511dc08296e16c493b20f988e5c978d10295
Jason Haynes <jason.haynes@gmail.com>
2024-11-10 15:18:42 -0500

    add grub.d file but do not yet auto-run update-grub

--- install-root.sh
+++ install-root.sh
@@ -13,3 +13,7 @@ fi
 # backups will be .~1~, .~2~, depending how many times it's run.  Investigate other options.
 cp -fr --preserve=mode,timestamps \
   --backup=numbered "${install_dir}"/root/* /
+
+# TODO only run update-grub when it's actually changed.
+# FIXME figure out which files to not backup because they'll be included!
+# FIXME or, better yet, place all backup files in a separate tree.
