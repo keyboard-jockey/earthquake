5fc1532444f5b95102ec006d6c11a0cde40d5d22
Jason Haynes <jason.haynes@gmail.com>
2024-11-11 12:58:41 -0500

    fixed mozilla.cfg locked settings

--- local-settings.js
+++
@@ -1,8 +0,0 @@
-pref("general.config.obscure_value", 0);
-pref("browser.newtabpage.enabled", true);
-pref("browser.sessionstore.max_resumed_crashes", -1);
-pref("browser.sessionrtore.resume_from_crash", false);
-pref("general.config.filename", "mozilla.cfg");
-pref("browser.startup.homepage", "file:///home/jason/Earthquake/www/barbarashomepage.html")
-pref("privacy.donottrackheader.enabled", true);
-pref("privacy.globalprivacycontrol.enabled", true);
--- root/usr/lib/firefox/defaults/pref/all.js
+++
@@ -1,3 +0,0 @@
-// # first line might be ignored by mozilla/firefox
-pref("general.config.obscure_value", 0);
-pref("general.config.filename", "mozilla.cfg");
